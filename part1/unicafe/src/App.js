692e8ba7e5dcbbba250b3c06a7a834ea302eb900
geoph9 <geoph9@protonmail.com>
2021-05-02 21:18:27 +0300

    unicafe step 2: add extra stats

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -16,6 +16,8 @@ const History = ({goods, neutrals, bads}) => {
   )
 }
 
+const StatContent = ({name, count}) => <p>{name}: {count}</p>
+
 const Button = ({handleClick, text}) => (
   <button onClick={handleClick}>
     {text}
@@ -29,10 +31,38 @@ const App = () => {
   const [goods, setGoods] = useState(0);
   const [neutrals, setNeutrals] = useState(0);
   const [bads, setBads] = useState(0);
+  const [allClicks, setAllClicks] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positives, setPositives] = useState(0);
 
-  const incrementGood = () => setGoods(goods + 1)
-  const incrementNeutral = () => setNeutrals(neutrals + 1)
-  const incrementBad = () => setBads(bads + 1)
+  const getAverage = (newGoods, newNeutrals, newBads) => 
+    (newGoods-newBads)/(newGoods+newNeutrals+newBads)
+  const getPositives = (newGoods, newNeutrals, newBads) => {
+    if (newGoods + newNeutrals + newBads === 0) return 0
+    return newGoods / (newGoods+newNeutrals+newBads)
+  }
+  const formatPositives = () => positives*100 + " %"
+
+  const incrementGood = () => {
+    // The increment does not take effect inside the function
+    // and so we still need to pass the incremented variable.
+    setAverage(getAverage(goods+1, neutrals, bads))
+    setPositives(getPositives(goods+1, neutrals, bads))
+    setGoods(goods + 1)
+    setAllClicks(allClicks + 1)
+  }
+  const incrementNeutral = () => {
+    setAverage(getAverage(goods, neutrals+1, bads))
+    setPositives(getPositives(goods, neutrals+1, bads))
+    setNeutrals(neutrals + 1)
+    setAllClicks(allClicks + 1)
+  }
+  const incrementBad = () => {
+    setAverage(getAverage(goods, neutrals, bads+1))
+    setPositives(getPositives(goods, neutrals, bads+1))
+    setBads(bads + 1)
+    setAllClicks(allClicks + 1)
+  }
 
   return (
     <div>
@@ -43,7 +73,12 @@ const App = () => {
       <Button handleClick={incrementBad} text='bad' />
       <br />
       <Header header={stats} />
-      <History goods={goods} neutrals={neutrals} bads={bads} />
+      <StatContent name='good' count={goods} />
+      <StatContent name='neutral' count={neutrals} />
+      <StatContent name='bad' count={bads} />
+      <StatContent name='all' count={allClicks} />
+      <StatContent name='average' count={average} />
+      <StatContent name='positive' count={formatPositives()} />
       </div>
   )
 }
