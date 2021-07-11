e511bffbbe9ed4bf357d720bf5a5816c4481f682
geoph9 <geoph9@protonmail.com>
2021-07-11 14:56:16 +0300

    part3.1-3.2: phonebook steps2,3: basics

---
+++ part3/phonebook/index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.get('/', (request, response) => {
+  response.send('<h1>Phonebook App!</h1>')
+})
+
+app.get('/info', (request, response) => {
+  let html = `
+    <p>Phonebook has info for ${persons.length} people.</p>
+    <p>${new Date()}</p>`
+  response.send(html);
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
