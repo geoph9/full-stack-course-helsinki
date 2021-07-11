f00eea6461143ee5ca6160752b5265c1c74f62d9
geoph9 <geoph9@protonmail.com>
2021-07-11 14:58:03 +0300

    part3.4-3.5 - phonebook steps4,5: delete, add functionality

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -59,7 +59,32 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
 
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log("Name:", body.name, "== NUMBER:", body.number)
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'Missing persons name or number.' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
