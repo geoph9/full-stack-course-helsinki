762aa8e298b4f4de7b704c5975ff771e062c88a4
geoph9 <geoph9@protonmail.com>
2021-12-17 18:33:25 +0200

    ex4.4-partly 4.5 - running tests

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -1,6 +1,34 @@
 const dummy = (blogs) => {
   return 1
 }
+
+const totalLikes = (list) => {
+  const reducer = (sum, item) => {
+    let likes = item.likes === undefined ? 0 : item.likes
+    return sum + likes
+  }
+  return list.length === 0
+    ? 0
+    : list.reduce(reducer, 0)
+}
+
+const favoriteBlog = (list) => {
+  const reducer = (currFavorite, item) => {
+    let currLikes = currFavorite.likes === undefined ? 0 : currFavorite.likes
+  	let likes = item.likes === undefined ? 0 : item.likes
+  	if (likes > currLikes) {
+  	  return {
+  	  	title: item.title,
+  	  	author: item.author,
+  	  	likes: likes,
+  	  }
+  	}
+  	return currFavorite
+  }
+  return list.length === 0
+    ? undefined
+    : list.reduce(reducer, list[0])
+}
   
 const average = (array) => {
   const reducer = (sum, item) => {
@@ -12,5 +40,7 @@ const average = (array) => {
 }
   
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
