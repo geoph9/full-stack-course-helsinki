e281b831902e34821f3ed11a00407fa0dfc9cd76
geoph9 <geoph9@protonmail.com>
2021-12-21 16:57:52 +0200

    4.8-4.12: added extra tests for blog list api

--- part4/blog_list/models/blog.js
+++ part4/blog_list/models/blog.js
@@ -8,34 +8,42 @@ const url = config.MONGODB_URI
 // console.log("Connecting to", url)
 
 mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
 }).then(() => {
-    logger.info('Connected to MongoDB')
+  logger.info('Connected to MongoDB')
 }).catch((error) => {
-    logger.error('Error connecting to MongoDB:', error.message)
+  logger.error('Error connecting to MongoDB:', error.message)
 })
 
 const blogSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        minLength: 3,
-    },
-    author: String,
-    url: String,
-    likes: Number
+  title: {
+    type: String,
+    required: true,
+    minLength: 3,
+  },
+  author: {
+    type: String,
+    required: true,
+    minLength: 2,
+  },
+  url: String,
+  likes: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
 })
 blogSchema.plugin(uniqueValidator)
 
 blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Blog', blogSchema)
--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -3,6 +3,108 @@ const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  
+  for (let note of helper.initialBlogs) {
+    let blogObject = new Blog(note)
+    await blogObject.save()
+  }
+})
+
+// Get returns correct amount of blog posts
+test('number of blogs in database after GET is legit.', async () => {
+  const allBlogs = await helper.blogsInDb()
+  expect(allBlogs).toHaveLength(helper.initialBlogs.length)
+
+})
+
+// Adding a new note
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+  
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+  
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  const titles = blogsAtEnd.map(b => b.title)
+  expect(titles).toContain(
+    'Go To Statement Considered Harmful'
+  )
+})
+
+// Check that the id is named `id` and not `_id`
+test('identifier name should be `id`', async () => {
+  const allBlogs = await helper.blogsInDb()
+
+  expect(allBlogs[0].id).toBeDefined()
+})
+
+describe('missingProperties', () => {
+  // Check that if likes is missing from post then it defaults to 0
+  test('when likes is missing it defaults to 0', async () => {
+    const newBlog = {
+      author: 'Tester',
+      title: 'Test',
+      url: 'http://example.com/blog'
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    const newLike = blogsAtEnd.filter(b => b.title === 'Test')[0].likes
+    expect(newLike).toEqual(0)
+  })
+  
+  test('blog without title is not added', async () => {
+    const newBlog = {
+      author: 'Tester'
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    // Check that size stays the same
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+  
+  test('blog without author is not added', async () => {
+    const newBlog = {
+      title: 'Test'
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+    
+    // Check that size stays the same
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+})
 
 test('blogs are returned as json', async () => {
   await api
